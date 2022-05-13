1032a09dc3b0ab00a29cd4ebeea1c4d8ad38ecf0
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-05-13 12:35:54 +0530

    Fixed Padding on Mobile

--- src/Components/Grid.tsx
+++ src/Components/Grid.tsx
@@ -4,7 +4,7 @@ const PageGrid = ({ children, heading }) => {
   return (
     <div className="flex flex-col overflow-scroll relative p-0">
       {heading && (
-        <h1 className=" text-md font-normal sm:text-3xl sm:font-medium p-8 top-0 fixed z-50 backdrop-filter bg-white bg-opacity-80 backdrop-blur-xl w-full pb-20 sm:pb-4 m-0 sm:text-left text-center">
+        <h1 className="text-md font-normal sm:text-3xl sm:font-medium p-4 sm:p-8 top-0 fixed z-50 backdrop-filter bg-white bg-opacity-80 backdrop-blur-xl w-full pb-20 sm:pb-4 m-0 sm:text-left text-center">
           {heading}
         </h1>
       )}
