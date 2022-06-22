7d58987eb0d8ca3aa75df26d70ca4abe8c894871
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-06-23 01:57:17 +0530

    Refactored TopBar

--- src/Components/App/OngoingPage.jsx
+++ src/Components/App/OngoingPage.jsx
@@ -1,34 +1,12 @@
-import React, { useContext } from "react";
-import { AppContext } from "../Context";
-
-import Button from "../Views/Button";
-
-import { SearchIcon, MenuIcon, AddIcon } from "../Views/Icons";
+import TopBar from "./TopBar";
 
 const OngoingPage = () => {
-  const { setSidebarVisibleMobile } = useContext(AppContext);
 
   return (
     <div className="content">
-      <div className="flex flex-row justify-between">
-        <Button
-          type="icon"
-          className="mobile-only"
-          onClick={() => setSidebarVisibleMobile(true)}
-        >
-          <MenuIcon className="w-6 h-6" />
-        </Button>
-
-        <div className="w-full gap-4 center">
-          <div className="input">
-            <SearchIcon />
-            <input placeholder="Search Ongoing" />
-          </div>
+      <TopBar />
         </div>
-        <Button type="primary" className="mobile-only">
-          <AddIcon className="w-6 h-6" />
-        </Button>
-      </div>
+      ))}
     </div>
   );
 };
