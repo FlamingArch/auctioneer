f3c3027bbd60df7280fbd2138304d009ba0eba56
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-06-27 23:14:31 +0530

    Wallet

--- src/Components/App/WalletPage.jsx
+++ src/Components/App/WalletPage.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { SearchIcon } from "../Views/Icons";
+import List from "../Views/List";
+import ListItem from "../Views/ListItem";
+import TopBar from "./TopBar";
 
 const WalletPage = () => {
   return (
     <div className="content">
-      <div className="center w-full">
-        <div className="input">
-          <SearchIcon />
-          <input placeholder="Search Wallet" />
-        </div>
+      <TopBar />
+      <div className="p-4">
+        <List
+          heading="Wallet Balance"
+          subtitle="View Recent Wallet Transactions in the Account section."
+        >
+          <ListItem>
+            <p>Indian Rupee (INR)</p>
+            <p className="opacity-60">{1021220.0}</p>
+          </ListItem>
+          <ListItem>
+            <p>US Dollar (USD)</p>
+            <p className="opacity-60">{2303.0}</p>
+          </ListItem>
+          <ListItem>
+            <p>Bitcoin (BTC)</p>
+            <p className="opacity-60">{1.2032}</p>
+          </ListItem>
+          <ListItem>
+            <p>Etherium (ETH)</p>
+            <p className="opacity-60">{224.32}</p>
+          </ListItem>
+        </List>
       </div>
     </div>
   );
