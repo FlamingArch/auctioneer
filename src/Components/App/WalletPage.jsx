f82921b444d75f178f8cf8216a88ff38d7f25127
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-06-27 23:39:50 +0530

    Wallet Sharing

--- src/Components/App/WalletPage.jsx
+++ src/Components/App/WalletPage.jsx
@@ -29,6 +29,15 @@ const WalletPage = () => {
             <p className="opacity-60">{224.32}</p>
           </ListItem>
         </List>
+        <div className="p-4"></div>
+        <List
+          heading="Shared With"
+          subtitle="People you have shared your Wallet will appear here."
+        >
+          <ListItem>
+            You have not shared your wallet with other people.
+          </ListItem>
+        </List>
       </div>
     </div>
   );
--- src/Components/Views/Chip.jsx
+++ src/Components/Views/Chip.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Chip = ({ children }) => {
   return (
-    <div className="px-3 py-1 bg-gray-200 rounded-full w-min h-min">
+    <div
+      style={{ background: "#f2f3f5" }}
+      className="px-3 py-1 rounded-lg w-min h-min"
+    >
       {children}
     </div>
   );
