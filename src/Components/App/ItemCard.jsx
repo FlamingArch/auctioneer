0ad8f89dafbfc45734282c79834760ac63bf74b8
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-07-02 00:46:22 +0530

    Added Dependencies, Updated Cards

--- src/Components/App/ItemCard.jsx
+++ src/Components/App/ItemCard.jsx
@@ -3,18 +3,38 @@ import Chip from "../Views/Chip";
 import Button from "../Views/Button";
 import { FavouritesIcon, ShoppingIcon } from "../Views/Icons";
 
+import _ from "lodash";
+import { motion } from "framer-motion";
+
 import noimg from "../../Resources/no-img.png";
 
 const ItemCard = ({ item }) => {
   return (
-    <div className="item-card h-min">
-      <img src={item.images[0] ?? noimg} className="rounded-t-xl" />
+    <motion.div
+      animate={{ opacity: [0, 1], translateY: [25, 0] }}
+      className="item-card h-min"
+    >
+      <img src={item.images[0] ?? noimg} className="w-full rounded-t-xl" />
       <div className="flex flex-col gap-3 p-4 border-bottom">
         <p className="text-2xl font-bold">{item.name}</p>
         <div className="px-3 py-1 bg-blue-200 rounded-lg w-min">
           {item.category}
         </div>
-        <p>{item.description ?? "No Description Provided"}</p>
+        <p>
+          {item.description.substring(0, 100) + "..." ??
+            "No Description Provided"}
+        </p>
+      </div>
+      <div className="flex gap-3 p-4 border-bottom">
+        <img
+          src={item.owner.profile_picture}
+          alt=""
+          className="w-12 h-12 rounded-full"
+        />
+        <div className="flex flex-col">
+          <p className="font-bold">{item.owner.name}</p>
+          <p className="">{item.owner.bio}</p>
+        </div>
       </div>
       <div className="flex flex-col gap-3 p-4 border-bottom">
         <div className="flex justify-between p-2">
@@ -42,12 +62,12 @@ const ItemCard = ({ item }) => {
             <ShoppingIcon />
             {item.active ? "New Bid" : "Sold"}
           </Button>
-          <Button type="primary" disabled={true}>
+          {/* <Button type="primary" disabled={true}>
             <FavouritesIcon />
-          </Button>
+          </Button> */}
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
